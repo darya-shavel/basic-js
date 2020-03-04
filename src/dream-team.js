bc26e8078c403501c83ad49447ec08bf6f5cd8c6
Darya Shavel <darya_shavel98@mail.ru>
2020-03-05 01:15:09 +0300

    feat : add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam( members ) {
+    if (!Array.isArray(members)) {
+        return false;
+    }
+    let arr = [];
+    for (let i = 0; i < members.length; i++) {
+        if (typeof members[i] === 'string') {
+            let element = members[i].trim();
+            arr.push(element[0].toUpperCase());
+           }
+    }
+    return arr.sort().join('');
+}
