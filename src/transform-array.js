7255ceed1b6753473fa5e0d1786f87de88fbdb94
Darya Shavel <darya_shavel98@mail.ru>
2020-03-19 18:56:53 +0300

    feat : solution transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,25 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform( arr ) {
+if (!Array.isArray(arr)) {
+    throw new Error();
+} 
+let array =[];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+        i++;
+    }else if (arr[i] === '--discard-prev') {
+        array.pop();
+    }else if (arr[i] === '--double-next') {
+        array.push(arr[i + 1])
+    }else if (arr[i] === '--double-prev') {
+        array.push(arr[i -1])
+    }else{
+        array.push(arr[i]);
+    }
+}
+for (let i = 0; i < array.length; i++) {
+    if (array[i] == undefined) {
+        array.splice(i,1);
+    }
+}
+return array;
 };
